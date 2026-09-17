f9de47a2dd781aee4ae6c7ea0c830a1bf7497b6d
Keshav Shukla <k.shukla@jacksonandfrank.com>
2026-09-17 17:10:45 +0530

    feat: add main navigation data

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,8 +1,8 @@
 import { NavigationItem } from '@/types/common';
 
 export const mainNavigation: NavigationItem[] = [
-  { name: 'Job Alert', href: '/job-alert' },
   { name: 'Blogs', href: '/blog' },
   { name: 'News', href: '/news' },
   { name: 'Vacancies', href: '/vacancies' },
+  { name: 'Job Alert', href: '/job-alert' },
 ];
